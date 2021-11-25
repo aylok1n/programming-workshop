6b953df9849f4f8009455a2b05b528f43e42468a
aylok1n <nikolya.blog@gmail.com>
2021-11-25 22:03:56 +0400

    Add task3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import { Routes, Route, Link, } from "react-router-dom";
 import Task2 from './Tasks/Task2';
+import Task3 from './Tasks/Task3';
 
 function App() {
   return (
     <div className="App">
       <nav>
         <Link to='Task2' >Task2</Link>
+        <Link to='Task3' >Task3</Link>
       </nav>
       <Routes>
-        <Route  path="/Task2" element={<Task2 />} />
+        <Route path="/Task2" element={<Task2 />} />
+        <Route path="/Task3" element={<Task3 />} />
       </Routes>
     </div>
   );
---
+++ src/Tasks/Task3.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import './styles.css'
+
+export const Task3 = (props) => {
+    const [x1, setx1] = useState(0)
+    const [x2, setx2] = useState(0)
+    const [N, setN] = useState(100)
+    const [h, seth] = useState(0)
+    const [res, setRes] = useState([])
+
+    function factorial(num) {
+        var rval = 1;
+        for (var i = 2; i <= num; i++)
+            rval = rval * i;
+        return rval;
+    }
+
+    const calculate = () => {
+        let H
+        if (h === 0) H = Number(((1 - 0.1) / 10))
+        else H = h
+        let x = Number(x1)
+        let res = []
+        for (let n = 0; n <= N; n++) {
+            if (x <= x2) {
+                let result = Math.pow(2 * x, n) / factorial(n)
+                let y = Math.exp(2 * x)
+                res.push(`при x = ${x} сумма = ${result} Y(x) = ${y}`)
+                x += H
+            }
+        }
+        setRes(res)
+    }
+
+    return (
+        <div className='center '>
+            <h1>Task3</h1>
+            <div className='row between'>
+                <div className='column'>
+                    <label>
+                        x1
+                        <input value={x1} onChange={e => setx1(Number(e.target.value))} type='number' ></input>
+                    </label>
+                    <label>
+                        x2
+                        <input value={x2} onChange={e => setx2(Number(e.target.value))} type='number' ></input>
+                    </label>
+                </div>
+                <div className='column'>
+                    <label>
+                        N
+                        <input value={N} onChange={e => setN(Number(e.target.value))} type='number' ></input>
+                    </label>
+                    <label>
+                        h
+                        <input value={h} onChange={e => seth(Number(e.target.value))} type='number' ></input>
+                    </label>
+                </div>
+            </div>
+            <button onClick={calculate}>Выполнить</button>
+            {res.map((i, index) => <div key={index} >{i}</div>)}
+        </div>
+    )
+}
+export default Task3
